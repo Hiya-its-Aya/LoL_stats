0e7e63b40e670f3895b91753dbe34cd9b7b31cd3
ayamoosa <ayamoosa98@gmail.com>
2023-01-12 09:25:53 -0800

    start on front to back connection

--- src/Signin.js
+++ src/Signin.js
@@ -3,30 +3,44 @@ import React from "react";
 
 const Signin = () => {
   
-  // const [data, setData] = React.useState(null);
-  
+  const [values, setValues] = React.useState({
+    email: '',
+    password: '',
+    league_username: ''
+  });
+  //create three handlers for recording changes of forms
+
   // React.useEffect(() => {
   //   }, []);
+  const postSignUp = () => {
+    fetch("http://localhost:3000/signup")
 
-    return (
-      <form>
-        <h2>Sign Up</h2> 
-        <br/ >
-        <label name = 'email'>Email: 
-          <input type='email' required placeholder="leagueisfun@gmail.com"/>
-        </label>  
-        <br />
-        <label name = 'password'>Password: 
-          <input type='password' required />
-        </label>  
-        <br />
-        <label name = 'riotID'> Summoner Username:
-          <input type='riotID' required placeholder="leagueSummoner"/>
-        </label>
-        <br />
-        <button >Sign Up</button>
-      </form>
-    )
+  }
+
+  return (
+    <form>
+      <h2>Sign Up</h2> 
+      <br/ >
+      <label name = 'email'>Email: 
+        <input type='email' required 
+        placeholder="leagueisfun@gmail.com"
+        value = {values.email}/>
+      </label>  
+      <br />
+      <label name = 'password'>Password: 
+        <input type='password' required 
+        value = {values.password}/>
+      </label>  
+      <br />
+      <label name = 'riotID'> Summoner Username:
+        <input type='riotID' required 
+        placeholder="leagueSummoner"
+        value = {values.league_username}/>
+      </label>
+      <br />
+      <button onClick={postSignUp}>Sign Up</button>
+    </form>
+  )
 }
 
 export default Signin
